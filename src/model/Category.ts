b947244a50746920f84fee3d3ab8b005658e4b3c
Samuel <ottonsam@gmail.com>
2022-09-01 23:45:31 -0300

    Listando as categorias

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,5 +1,12 @@
 import { v4 as uuidV4 } from "uuid";
 
+interface ICategory {
+    id: string;
+    name: string;
+    description: string;
+    created_at: Date;
+};
+
 class Category {
     id?: string;
     name: string;
@@ -10,6 +17,9 @@ class Category {
         if(!this.id){
             this.id = uuidV4();
         }
+        this.name = "";
+        this.description = "";
+        this.created_at = new Date();
     }
 }
 
