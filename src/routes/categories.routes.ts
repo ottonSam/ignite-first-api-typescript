0c1ae44209e4445686ee9fb75e7f574f89f5ad4a
Samuel <ottonsam@gmail.com>
2022-09-23 23:28:49 -0300

    Atualizando a chamada as rotas

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,8 @@
-import { request, Router } from "express";
-import { CreateCategoryController } from "../modules/cars/controllers/CreateCategoryController";
-import { ListCategoryController } from "../modules/cars/controllers/ListCategoryController";
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { Router } from "express";
+import { createCategoryController } from "../modules/cars/useCases/CreateCategoryUseCase";
+import { listCategoryController } from "../modules/cars/useCases/ListCategoryUseCase";
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
-const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-const createCategoryController = new CreateCategoryController(createCategoryService);
-const listCategoryController = new ListCategoryController(categoriesRepository);
 
 categoriesRoutes.post('/', (request, response) => {
     return createCategoryController.handle(request, response);
