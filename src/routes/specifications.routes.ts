0114faac09eb539a9c0a6dc2a8fe5d787d896683
Samuel <ottonsam@gmail.com>
2022-09-23 23:37:44 -0300

    usando o use case

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,20 +1,9 @@
 import { Router } from 'express';
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { createSpecificationController } from '../modules/cars/useCases/CreateSpecificationsUseCase';
 
 const specificationsRoutes = Router();
 
-const specificationRepository = new SpecificationsRepository();
-
 specificationsRoutes.post('/', (request, response) => {
-    const {name, description} = request.body;
-
-    const createCategoryService = new CreateSpecificationService(specificationRepository);
-    createCategoryService.execute({
-        name,
-        description
-    });
-
-    return response.status(201).send();
+    return createSpecificationController.handle(request, response);
 });
 export { specificationsRoutes };
