65f90e09735e16b96e3db7d0dc45c3b5ffc6a76e
Samuel <ottonsam@gmail.com>
2022-09-24 12:11:48 -0300

    atualizando as rotas com as novas funções

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { createSpecificationController } from '../modules/cars/useCases/CreateSpecificationsUseCase';
+import { listSpecificationController } from '../modules/cars/useCases/ListSpecificationsUseCase';
 
 const specificationsRoutes = Router();
 
 specificationsRoutes.post('/', (request, response) => {
     return createSpecificationController.handle(request, response);
 });
+
+specificationsRoutes.get('/', (request, response) => {
+    return listSpecificationController.handle(request, response);
+});
+
 export { specificationsRoutes };
