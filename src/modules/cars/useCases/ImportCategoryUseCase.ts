c500a91a419794da3d1747b3c4d83f7d44ee5f92
Samuel <ottonsam@gmail.com>
2022-10-16 00:20:52 -0300

    reading the file using stream

--- src/modules/cars/useCases/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/ImportCategoryUseCase.ts
@@ -1,14 +1,24 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
 import { ImportCategoryController } from "../controllers/ImportCategoryController";
 
 class ImportCategoryUseCase {
     execute(file: any) {
-        console.log(file);
+        const stream = fs.createReadStream(file.path);
+
+        const parseFile = parse();
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
+
     }
 }
 
 const importCategoryUseCase = new ImportCategoryUseCase();
-const importCategoryController = new ImportCategoryController(
-    importCategoryUseCase
-);
+const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
 export { importCategoryController, ImportCategoryUseCase }
