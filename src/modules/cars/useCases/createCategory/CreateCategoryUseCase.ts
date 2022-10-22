2e538630c435d791af23d6a6da6299e78aa5a56d
Samuel <ottonsam@gmail.com>
2022-10-22 01:14:55 -0300

    use tsyringe on create category

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,22 +1,26 @@
-import { response } from "express";
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+import { inject, injectable } from "tsyringe";
+
 import { ICategoryRepository } from "../../repositories/ICategoriesRepository";
 interface IRequest {
     name: string;
     description: string;
 }
 
+@injectable()
 class CreateCategoryUseCase {
-    constructor(private categoriesRepository: ICategoryRepository) {}
+    constructor(
+        @inject("CategoriesRepository")
+        private categoriesRepository: ICategoryRepository
+    ) { }
 
-    async execute({name, description}: IRequest): Promise<void> {
+    async execute({ name, description }: IRequest): Promise<void> {
         const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
 
-        if(categoryAlreadyExists){
-            throw new Error(`Category ${name} already exists`);    
+        if (categoryAlreadyExists) {
+            throw new Error(`Category ${name} already exists`);
         }
 
-        this.categoriesRepository.create({name, description});
+        this.categoriesRepository.create({ name, description });
     }
 }
 
