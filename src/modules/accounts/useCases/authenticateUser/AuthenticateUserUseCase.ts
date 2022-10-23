62483384b72d5e03c9f2b149543d3dd7e3eecb94
Samuel <ottonsam@gmail.com>
2022-10-23 06:38:36 -0300

    authenticate user function

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { inject, injectable } from "tsyringe";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    },
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error("Email or password incorrect");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email or password incorrect");
+        }
+
+        const token = sign({}, "2d17494838cf26d6cbbbe5ee86221db4", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        return {
+            user: {
+                name: user.name,
+                email: user.email
+            },
+            token
+        }
+    }
+}
+
+export { AuthenticateUserUseCase };
